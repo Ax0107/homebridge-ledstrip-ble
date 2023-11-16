a5f3129e1c9aaf29f453b948c0de74d81892c957
Александр Шупов <avshupov@quasarpro.ru>
2023-11-16 03:44:09 +0300

    fix

--- rgb_control.py
+++ rgb_control.py
@@ -97,8 +97,9 @@ async def connect():
     if CONNECTING:
         print('[RGB] already started connect')
         Attempts += 1
-        if Attempts == 10:
+        if Attempts > 15:
             CONNECTING = False
+            Attempts = 0
         else:
             return
     
